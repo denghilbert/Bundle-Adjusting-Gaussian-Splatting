3b8dbdde18970ff9d1acac5c159cbedde30a5040
denghilbert <971431670@qq.com>
2023-12-30 00:38:17 -0500

    Init projection_test

---
+++ projection_test.py
@@ -0,0 +1,241 @@
+
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+import os
+import torch
+from random import randint
+from utils.loss_utils import l1_loss, ssim, kl_divergence, l2_loss
+from gaussian_renderer import render, network_gui
+import sys
+from scene import Scene, GaussianModel, SpecularModel
+from utils.general_utils import safe_state, get_linear_noise_func, linear_to_srgb
+import uuid
+from tqdm import tqdm
+from utils.image_utils import psnr
+from utils.visualization import wandb_image
+from argparse import ArgumentParser, Namespace
+from arguments import ModelParams, PipelineParams, OptimizationParams
+try:
+    from torch.utils.tensorboard import SummaryWriter
+    TENSORBOARD_FOUND = True
+except ImportError:
+    TENSORBOARD_FOUND = False
+
+import wandb
+
+# set random seeds
+import numpy as np
+import random
+seed_value = 42  # Replace this with your desired seed value
+
+torch.manual_seed(seed_value)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)  # if you are using multi-GPU.
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+np.random.seed(seed_value)
+random.seed(seed_value)
+
+def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoint_iterations, checkpoint, debug_from, use_wandb=False, random_init=False, hybrid=False, opt_cam=False, r_t_noise=[0., 0.]):
+    first_iter = 0
+    tb_writer = prepare_output_and_logger(dataset)
+    gaussians = GaussianModel(dataset.sh_degree, dataset.asg_degree)
+    if hybrid:
+        specular_mlp = SpecularModel()
+        specular_mlp.train_setting(opt)
+
+    scene = Scene(dataset, gaussians, random_init=random_init, r_t_noise=r_t_noise)
+    gaussians.training_setup(opt)
+    if checkpoint:
+        (model_params, first_iter) = torch.load(checkpoint)
+        gaussians.restore(model_params, opt)
+    bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
+    background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+    mlp_color = 0
+
+    viewpoint_stack = scene.getTrainCameras().copy()
+    viewpoint_cam = viewpoint_stack.pop(randint(0, len(viewpoint_stack)-1))
+
+
+    render_pkg = render(viewpoint_cam, gaussians, pipe, background, mlp_color, iteration=7000, hybrid=False)
+    image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
+    gt_image = viewpoint_cam.original_image.cuda()
+
+    wandb_img = image.unsqueeze(0).detach()
+    wandb_img_gt = gt_image.unsqueeze(0).detach()
+    images_error = (wandb_img_gt - wandb_img).abs()
+    images = {
+        f"vis/rgb_target": wandb_image(gt_image),
+        f"vis/rgb_render": wandb_image(wandb_img),
+        f"vis/rgb_error": wandb_image(images_error),
+    }
+    wandb.log(images, step=0)
+
+def prepare_output_and_logger(args):
+    if not args.model_path:
+        if os.getenv('OAR_JOB_ID'):
+            unique_str=os.getenv('OAR_JOB_ID')
+        else:
+            unique_str = str(uuid.uuid4())
+        args.model_path = os.path.join("./output/", unique_str[0:10])
+
+    # Set up output folder
+    print("Output folder: {}".format(args.model_path))
+    os.makedirs(args.model_path, exist_ok = True)
+    with open(os.path.join(args.model_path, "cfg_args"), 'w') as cfg_log_f:
+        cfg_log_f.write(str(Namespace(**vars(args))))
+
+    # Create Tensorboard writer
+    tb_writer = None
+    if TENSORBOARD_FOUND:
+        tb_writer = SummaryWriter(args.model_path)
+    else:
+        print("Tensorboard not available: not logging progress")
+    return tb_writer
+
+def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_iterations, scene : Scene, renderFunc, renderArgs):
+    if tb_writer:
+        tb_writer.add_scalar('train_loss_patches/l1_loss', Ll1.item(), iteration)
+        tb_writer.add_scalar('train_loss_patches/total_loss', loss.item(), iteration)
+        tb_writer.add_scalar('iter_time', elapsed, iteration)
+
+    # Report test and samples of training set
+    if iteration in testing_iterations:
+        torch.cuda.empty_cache()
+        validation_configs = ({'name': 'test', 'cameras' : scene.getTestCameras()},
+                              {'name': 'train', 'cameras' : [scene.getTrainCameras()[idx % len(scene.getTrainCameras())] for idx in range(5, 30, 5)]})
+
+        for config in validation_configs:
+            if config['cameras'] and len(config['cameras']) > 0:
+                l1_test = 0.0
+                psnr_test = 0.0
+                for idx, viewpoint in enumerate(config['cameras']):
+                    image = torch.clamp(renderFunc(viewpoint, scene.gaussians, *renderArgs)["render"], 0.0, 1.0)
+                    gt_image = torch.clamp(viewpoint.original_image.to("cuda"), 0.0, 1.0)
+                    if tb_writer and (idx < 5):
+                        tb_writer.add_images(config['name'] + "_view_{}/render".format(viewpoint.image_name), image[None], global_step=iteration)
+                        if iteration == testing_iterations[0]:
+                            tb_writer.add_images(config['name'] + "_view_{}/ground_truth".format(viewpoint.image_name), gt_image[None], global_step=iteration)
+                    l1_test += l1_loss(image, gt_image).mean().double()
+                    psnr_test += psnr(image, gt_image).mean().double()
+                psnr_test /= len(config['cameras'])
+                l1_test /= len(config['cameras'])
+                print("\n[ITER {}] Evaluating {}: L1 {} PSNR {}".format(iteration, config['name'], l1_test, psnr_test))
+                if tb_writer:
+                    tb_writer.add_scalar(config['name'] + '/loss_viewpoint - l1_loss', l1_test, iteration)
+                    tb_writer.add_scalar(config['name'] + '/loss_viewpoint - psnr', psnr_test, iteration)
+
+        if tb_writer:
+            tb_writer.add_histogram("scene/opacity_histogram", scene.gaussians.get_opacity, iteration)
+            tb_writer.add_scalar('total_points', scene.gaussians.get_xyz.shape[0], iteration)
+        torch.cuda.empty_cache()
+
+
+def init_wandb(cfg, wandb_id=None, project="", run_name=None, mode="online", resume=False, use_group=False, set_group=None):
+    r"""Initialize Weights & Biases (wandb) logger.
+
+    Args:
+        cfg (obj): Global configuration.
+        wandb_id (str): A unique ID for this run, used for resuming.
+        project (str): The name of the project where you're sending the new run.
+            If the project is not specified, the run is put in an "Uncategorized" project.
+        run_name (str): name for each wandb run (useful for logging changes)
+        mode (str): online/offline/disabled
+    """
+    print('Initialize wandb')
+    if not wandb_id:
+        wandb_path = os.path.join(cfg.model_path, "wandb_id.txt")
+        if resume and os.path.exists(wandb_path):
+            with open(wandb_path, "r") as f:
+                wandb_id = f.read()
+        else:
+            wandb_id = wandb.util.generate_id()
+            with open(wandb_path, "w") as f:
+                f.write(wandb_id)
+    if use_group:
+        group, name = cfg.model_path.split("/")[-2:]
+        group = set_group
+    else:
+        group, name = None, os.path.basename(cfg.model_path)
+        group = set_group
+
+    if run_name is not None:
+        name = run_name
+    wandb.init(id=wandb_id,
+               project=project,
+               config=vars(cfg),
+               group=group,
+               name=name,
+               dir=cfg.model_path,
+               resume=resume,
+               settings=wandb.Settings(start_method="fork"),
+               mode=mode)
+    wandb.config.update({'dataset': cfg.source_path})
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Training script parameters")
+    lp = ModelParams(parser)
+    op = OptimizationParams(parser)
+    pp = PipelineParams(parser)
+    parser.add_argument('--ip', type=str, default="127.0.0.1")
+    parser.add_argument('--port', type=int, default=6009)
+    parser.add_argument('--debug_from', type=int, default=-1)
+    parser.add_argument('--detect_anomaly', action='store_true', default=False)
+    parser.add_argument("--test_iterations", nargs="+", type=int, default=[7_000, 30_000])
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 30_000])
+    parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
+    parser.add_argument("--start_checkpoint", type=str, default = None)
+    # wandb setting
+    parser.add_argument("--wandb", action="store_true", default=False)
+    parser.add_argument("--wandb_project_name", type=str, default = None)
+    parser.add_argument("--wandb_group_name", type=str, default = None)
+    parser.add_argument("--wandb_mode", type=str, default = "online")
+    parser.add_argument("--resume", action="store_true", default=False)
+    # random init point cloud
+    parser.add_argument("--random_init_pc", action="store_true", default=False)
+
+    # use hybrid for specular
+    parser.add_argument("--hybrid", action="store_true", default=False)
+    # if optimize camera poses
+    parser.add_argument("--opt_cam", action="store_true", default=False)
+    # noise for rotation and translation
+    parser.add_argument("--r_t_noise", nargs="+", type=float, default=[0., 0.])
+
+    args = parser.parse_args(sys.argv[1:])
+    args.save_iterations.append(args.iterations)
+    print("Optimizing " + args.model_path)
+
+    # Initialize wandb
+    if args.wandb:
+        wandb.login()
+        wandb_run = init_wandb(args,
+                               project=args.wandb_project_name,
+                               mode=args.wandb_mode,
+                               resume=args.resume,
+                               use_group=True,
+                               set_group=args.wandb_group_name
+                               )
+
+    # Initialize system state (RNG)
+    safe_state(args.quiet)
+
+    # Start GUI server, configure and run training
+    network_gui.init(args.ip, args.port)
+    torch.autograd.set_detect_anomaly(args.detect_anomaly)
+    training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from, use_wandb=args.wandb, random_init=args.random_init_pc, hybrid=args.hybrid, opt_cam=args.opt_cam, r_t_noise=args.r_t_noise)
+
+    # All done
+    print("\nTraining complete.")
