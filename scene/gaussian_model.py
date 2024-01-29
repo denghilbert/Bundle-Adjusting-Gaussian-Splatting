5357b0b108425b0a5f589aa731e3011fc116fa52
denghilbert <971431670@qq.com>
2024-01-28 23:42:39 -0500

    Change back to original save_ply to use viewer

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -275,15 +275,15 @@ class GaussianModel:
         mkdir_p(os.path.dirname(path))
 
         xyz = self._xyz.detach().cpu().numpy()
-        #normals = np.zeros_like(xyz)
-        normals = self._normal.detach().cpu().numpy()
-        normals2 = self._normal2.detach().cpu().numpy()
+        normals = np.zeros_like(xyz)
+        #normals = self._normal.detach().cpu().numpy()
+        #normals2 = self._normal2.detach().cpu().numpy()
         f_dc = self._features_dc.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
         f_rest = self._features_rest.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
         opacities = self._opacity.detach().cpu().numpy()
         scale = self._scaling.detach().cpu().numpy()
         rotation = self._rotation.detach().cpu().numpy()
-        f_asg = self._features_asg.detach().cpu().numpy()
+        #f_asg = self._features_asg.detach().cpu().numpy()
 
         dtype_full = [(attribute, 'f4') for attribute in self.construct_list_of_attributes()]
 
