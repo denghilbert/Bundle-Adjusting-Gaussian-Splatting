6dd5782f6996094bf8e4e41f5c8b6c47b6a3eb80
denghilbert <971431670@qq.com>
2024-02-23 01:07:23 -0500

    Add test-time testset optimization

--- render.py
+++ render.py
@@ -24,6 +24,19 @@ from gaussian_renderer import GaussianModel
 import imageio
 import numpy as np
 import cv2
+import visdom
+from easydict import EasyDict
+
+from utils.util import check_socket_open
+from utils.util_vis import vis_cameras
+from random import randint
+from utils.loss_utils import l1_loss, ssim, kl_divergence, l2_loss
+
+opt_vis = EasyDict({'group': 'exp_synthetic', 'name': 'l2g_lego', 'model': 'l2g_nerf', 'yaml': 'l2g_nerf_blender', 'seed': 0, 'gpu': 0, 'cpu': False, 'load': None, 'arch': {'layers_feat': [None, 256, 256, 256, 256, 256, 256, 256, 256], 'layers_rgb': [None, 128, 3], 'skip': [4], 'posenc': {'L_3D': 10, 'L_view': 4}, 'density_activ': 'softplus', 'tf_init': True, 'layers_warp': [None, 256, 256, 256, 256, 256, 256, 6], 'skip_warp': [4], 'embedding_dim': 128}, 'data': {'root': '/the/data/path/of/nerf_synthetic/', 'dataset': 'blender', 'image_size': [400, 400], 'num_workers': 4, 'preload': True, 'augment': {}, 'center_crop': None, 'val_on_test': False, 'train_sub': None, 'val_sub': 4, 'scene': 'lego', 'bgcolor': 1}, 'loss_weight': {'render': 0, 'render_fine': None, 'global_alignment': 2}, 'optim': {'lr': 0.0005, 'lr_end': 0.0001, 'algo': 'Adam', 'sched': {'type': 'ExponentialLR', 'gamma': None}, 'lr_pose': 0.001, 'lr_pose_end': 1e-08, 'sched_pose': {'type': 'ExponentialLR', 'gamma': None}, 'warmup_pose': None, 'test_photo': True, 'test_iter': 100}, 'batch_size': None, 'max_epoch': None, 'resume': False, 'output_root': 'output', 'tb': {'num_images': [4, 8]}, 'visdom': {'server': 'localhost', 'port': 8600, 'cam_depth': 0.5}, 'freq': {'scalar': 200, 'vis': 1000, 'val': 2000, 'ckpt': 5000}, 'nerf': {'view_dep': True, 'depth': {'param': 'metric', 'range': [2, 6]}, 'sample_intvs': 128, 'sample_stratified': True, 'fine_sampling': False, 'sample_intvs_fine': None, 'rand_rays': 1024, 'density_noise_reg': None, 'setbg_opaque': False}, 'camera': {'model': 'perspective', 'ndc': False, 'noise': True, 'noise_r': 0.07, 'noise_t': 0.5}, 'max_iter': 200000, 'trimesh': {'res': 128, 'range': [-1.2, 1.2], 'thres': 25.0, 'chunk_size': 16384}, 'barf_c2f': [0.1, 0.5], 'error_map_size': None, 'output_path': 'output/exp_synthetic/l2g_lego', 'device': 'cuda:0', 'H': 400, 'W': 400})
+is_open = check_socket_open(opt_vis.visdom.server,opt_vis.visdom.port)
+vis = visdom.Visdom(server=opt_vis.visdom.server,port=opt_vis.visdom.port,env=opt_vis.group)
+
+
 
 def render_set(model_path, name, iteration, views, gaussians, pipeline, background, specular=None, hybrid=False):
     render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
@@ -54,26 +67,63 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
         torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
         #torchvision.utils.save_image(depth, os.path.join(depth_path, '{0:05d}'.format(idx) + ".png"))
 
-def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, mode: str, hybrid: bool):
-    with torch.no_grad():
-        gaussians = GaussianModel(dataset.sh_degree, dataset.asg_degree)
-        scene = Scene(dataset, gaussians, load_iteration=iteration, shuffle=False)
-        specular = None
-        if hybrid:
-            specular = SpecularModel()
-            specular.load_weights(dataset.model_path)
+def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, mode: str, hybrid: bool, opt_test_cam: bool):
+    gaussians = GaussianModel(dataset.sh_degree, dataset.asg_degree)
+    scene = Scene(dataset, gaussians, load_iteration=iteration, shuffle=False)
+    specular = None
+    if hybrid:
+        specular = SpecularModel()
+        specular.load_weights(dataset.model_path)
 
-        bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
-        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+    bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
+    background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
-        render_func = render_set
+    render_func = render_set
 
-        scene.loadAlignCameras(if_vis_test=True)
-        if not skip_train:
-             render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, specular, hybrid)
+    scene.loadAlignCameras(if_vis_test=True)
 
-        if not skip_test:
-             render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background, specular, hybrid)
+    viewpoint_stack = scene.getTestCameras().copy()
+    progress_bar = tqdm(range(0, 10000), desc="Training progress")
+    if opt_test_cam:
+        for iteration in range(10000):
+            if iteration % 200 == 0:
+                pose_gt, pose_aligned = scene.visTestCameras()
+                vis_cameras(opt_vis, vis, iteration, poses=[pose_aligned, pose_gt])
+            if not viewpoint_stack:
+                viewpoint_stack = scene.getTestCameras().copy()
+            viewpoint_cam = viewpoint_stack.pop(randint(0, len(viewpoint_stack)-1))
+
+            mlp_color = 0
+            render_pkg = render(viewpoint_cam, gaussians, pipeline, background, mlp_color, iteration=iteration, hybrid=hybrid, global_alignment=scene.getGlobalAlignment())
+            image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
+            gt_image = viewpoint_cam.original_image.cuda()
+            Ll1 = l1_loss(image, gt_image)
+            ssim_loss = ssim(image, gt_image)
+            loss = 0.8 * Ll1 + 0.2 * (1.0 - ssim_loss)
+            loss.backward(retain_graph=True)
+
+
+            with torch.no_grad():
+                if iteration % 10 == 0:
+                    progress_bar.set_postfix({"Loss": f"{loss.item():.{7}f}"})
+                    progress_bar.update(10)
+                if iteration == 10000:
+                    progress_bar.close()
+
+                scene.optimizer_rotation_test.step()
+                scene.optimizer_translation_test.step()
+                scene.optimizer_rotation_test.zero_grad(set_to_none=True)
+                scene.optimizer_translation_test.zero_grad(set_to_none=True)
+                scene.scheduler_rotation_test.step()
+                scene.scheduler_translation_test.step()
+
+
+
+    if not skip_train:
+         render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background, specular, hybrid)
+
+    if not skip_test:
+         render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background, specular, hybrid)
 
 if __name__ == "__main__":
     # Set up command line argument parser
@@ -86,10 +136,11 @@ if __name__ == "__main__":
     parser.add_argument("--hybrid", action="store_true", default=False)
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--mode", default='render', choices=['render', 'view', 'all', 'pose', 'original'])
+    parser.add_argument("--opt_test_cam", action="store_true", default=False)
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
 
     # Initialize system state (RNG)
     safe_state(args.quiet)
 
-    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args.mode, args.hybrid)
+    render_sets(model.extract(args), args.iteration, pipeline.extract(args), args.skip_train, args.skip_test, args.mode, args.hybrid, args.opt_test_cam)
--- scene/__init__.py
+++ scene/__init__.py
@@ -68,10 +68,13 @@ class Scene:
 
         self.train_cameras = {}
         self.unnoisy_train_cameras = {}
+        self.unnoisy_test_cameras = {}
+        self.debug_cameras = {}
         self.test_cameras = {}
         self.global_translation = nn.Parameter(torch.tensor([1.]).cuda().requires_grad_(True))
         self.global_quaternion = nn.Parameter(torch.tensor([1., 0, 0, 0]).cuda().requires_grad_(True))
         self.global_rotation = quaternion_to_rotation_matrix(self.global_quaternion)
+        self.sim3 = None
 
         if os.path.exists(os.path.join(args.source_path, "sparse")):
             scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
@@ -110,9 +113,12 @@ class Scene:
 
             generator = torch.Generator().manual_seed(55)
             self.unnoisy_train_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.train_cameras, resolution_scale, args)
+            self.debug_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.train_cameras, resolution_scale, args)
             # simply add noise
             so3_noise = torch.randn((len(scene_info.train_cameras), 3), generator=generator) * r_t_noise[0]
             t_noise = (torch.randn((len(scene_info.train_cameras), 3), generator=generator) * r_t_noise[1]).numpy()
+            #so3_noise = torch.randn((len(scene_info.train_cameras), 3)) * r_t_noise[0]
+            #t_noise = (torch.randn((len(scene_info.train_cameras), 3)) * r_t_noise[1]).numpy()
 
             # add systematic rotation and translation noise to verify preAlignment function
             #so3_noise = torch.ones(len(scene_info.train_cameras), 3) * r_t_noise[0]
@@ -127,7 +133,7 @@ class Scene:
                 tmp_R = so3[index] @ scene_info.train_cameras[index].R
                 #tmp_T = so3[index] @ scene_info.train_cameras[index].T + t_noise[index]
                 #tmp_T = so3[index] @ (scene_info.train_cameras[index].T + t_noise[index])
-                tmp_T = (scene_info.train_cameras[index].T + t_noise[index])
+                tmp_T = scene_info.train_cameras[index].T + t_noise[index]
                 scene_info.train_cameras[index] = scene_info.train_cameras[index]._replace(T=tmp_T, R=tmp_R)
                 #import pdb; pdb.set_trace()
                 #print(scene_info.train_cameras[index])
@@ -135,6 +141,7 @@ class Scene:
             self.train_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.train_cameras, resolution_scale, args)
             print("Loading Test Cameras")
             self.test_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.test_cameras, resolution_scale, args)
+            self.unnoisy_test_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.test_cameras, resolution_scale, args)
 
         if self.loaded_iter:
             self.gaussians.load_ply(os.path.join(self.model_path, "point_cloud", "iteration_" + str(self.loaded_iter), "point_cloud.ply"))
@@ -144,10 +151,12 @@ class Scene:
         # naive implementation to deal with pose noise
         # set camera parameters as learnbale parameters
         l_rotation = [{'params': camera.delta_quaternion, 'lr': r_t_lr[0]} for camera in self.train_cameras[resolution_scale]]
+        l_rotation_test = [{'params': camera.delta_quaternion, 'lr': 0.001} for camera in self.test_cameras[resolution_scale]]
         #l_rotation = [{'params': camera.quaternion, 'lr': r_t_lr[0]} for camera in self.train_cameras[resolution_scale]]
         #l_rotation = [{'params': camera.so3, 'lr': 0.01} for camera in self.train_cameras[resolution_scale]]
 
         l_translation = [{'params': camera.delta_translation, 'lr': r_t_lr[1]} for camera in self.train_cameras[resolution_scale]]
+        l_translation_test = [{'params': camera.delta_translation, 'lr': 0.005} for camera in self.test_cameras[resolution_scale]]
         #l_translation = [{'params': camera.delta_translation_xy, 'lr': r_t_lr[1]} for camera in self.train_cameras[resolution_scale]]
         #l_translation = [{'params': camera.delta_translation_z, 'lr': r_t_lr[1]} for camera in self.train_cameras[resolution_scale]]
         #l_translation = [{'params': camera.translation, 'lr': r_t_lr[1]} for camera in self.train_cameras[resolution_scale]]
@@ -157,9 +166,18 @@ class Scene:
         self.scheduler_rotation = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_rotation, milestones=[7000, 50000], gamma=1)
         self.scheduler_translation = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_translation, milestones=[7000, 50000], gamma=1)
 
+        self.optimizer_rotation_test = torch.optim.Adam(l_rotation_test, eps=1e-15)
+        self.optimizer_translation_test = torch.optim.Adam(l_translation_test, eps=1e-15)
+        self.scheduler_rotation_test = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_rotation_test, milestones=[7000, 50000], gamma=1)
+        self.scheduler_translation_test = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_translation_test, milestones=[7000, 50000], gamma=1)
+
         l_global_alignment = [{'params': self.global_translation, 'lr': global_alignment_lr}, {'params': self.global_quaternion, 'lr': global_alignment_lr}]
         self.optimizer_global_alignment = torch.optim.Adam(l_global_alignment, eps=1e-15)
         self.scheduler_global_aligment = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_rotation, milestones=[7000, 50000], gamma=1)
+
+        #self.test_cameras[resolution_scale] = self.train_cameras[resolution_scale][90:]
+        #self.train_cameras[resolution_scale] = self.train_cameras[resolution_scale][:90]
+        #self.unnoisy_train_cameras[resolution_scale] = self.unnoisy_train_cameras[resolution_scale][:90]
 
     def save(self, iteration):
         point_cloud_path = os.path.join(self.model_path, "point_cloud/iteration_{}".format(iteration))
@@ -171,7 +189,43 @@ class Scene:
     def get_unnoisy_TrainCameras(self, scale=1.0):
         return self.unnoisy_train_cameras[scale]
 
+    def get_unnoisy_TestCameras(self, scale=1.0):
+        return self.unnoisy_test_cameras[scale]
+
     def getTestCameras(self, scale=1.0):
+        #if self.sim3 != None:
+        #    sim3 = self.sim3
+        #    center_pred = torch.stack([camera.get_camera_center() for camera in self.getTrainCameras()], dim=0) # [N,3]
+        #    center_GT = torch.stack([camera.get_camera_center() for camera in self.get_unnoisy_TrainCameras()], dim=0) # [N,3]
+        #    try:
+        #        sim3 = procrustes_analysis(center_GT,center_pred)
+        #        self.sim3 = sim3
+        #    except:
+        #        print("warning: SVD did not converge...")
+        #        sim3 = edict(t0=0,t1=0,s0=1,s1=1,R=torch.eye(3,device='cuda'))
+        #        self.sim3 = sim3
+        #    center_pred2gt = (center_pred - sim3.t1) / sim3.s1@sim3.R.t()*sim3.s0 + sim3.t0
+        #    center_gt2pred = (center_GT - sim3.t0) / sim3.s0@sim3.R*sim3.s1 + sim3.t1
+
+        #    R_pred = torch.stack([camera.get_w2c[:3, :3].t() for camera in self.getTrainCameras()], dim=0)
+        #    R_gt = torch.stack([camera.get_w2c[:3, :3].t() for camera in self.get_unnoisy_TrainCameras()], dim=0)
+        #    t_pred = torch.stack([camera.get_w2c[:3, 3:] for camera in self.getTrainCameras()], dim=0)
+        #    t_gt = torch.stack([camera.get_w2c[:3, 3:] for camera in self.get_unnoisy_TrainCameras()], dim=0)
+
+        #    R_pred2gt = R_pred @ sim3.R.t()
+        #    R_gt2pred = R_gt @ sim3.R
+        #    # we can use this to find t
+        #    t_pred2gt = (-R_pred2gt.transpose(1, 2) @ center_pred2gt[..., None])
+        #    t_gt2pred = (-R_gt2pred.transpose(1, 2) @ center_gt2pred[..., None])
+        #    self.unnoisy_train_cameras[scale][0].get_w2c[:3, :3]
+        #    self.train_cameras[scale][0].get_w2c[:3, :3]
+        #    copy_gt2pred = self.debug_cameras[scale]
+        #    #for camera, R, t in zip(copy_gt2pred, R_gt2pred, t_gt2pred):
+        #    for camera, R, t in zip(copy_gt2pred, R_pred, t_pred):
+        #        camera.reset_extrinsic(R.cpu().numpy(), t.cpu().numpy())
+
+
+        #return copy_gt2pred
         return self.test_cameras[scale]
 
     def getGlobalAlignment(self):
@@ -179,7 +233,16 @@ class Scene:
         return self.global_rotation, self.global_translation
 
     @torch.no_grad()
-    def loadAlignCameras(self, if_vis_train=False, if_vis_test=False, camera_uid_list=[]):
+    def visTestCameras(self):
+        R_pred = torch.stack([camera.get_w2c[:3, :3].t() for camera in self.getTestCameras()], dim=0)
+        R_gt = torch.stack([camera.get_w2c[:3, :3].t() for camera in self.get_unnoisy_TestCameras()], dim=0)
+        t_pred = torch.stack([camera.get_w2c[:3, 3:] for camera in self.getTestCameras()], dim=0)
+        t_gt = torch.stack([camera.get_w2c[:3, 3:] for camera in self.get_unnoisy_TestCameras()], dim=0)
+
+        return torch.cat((R_gt.transpose(1, 2), t_gt), dim=-1), torch.cat((R_pred.transpose(1, 2), t_pred), dim=-1)
+
+    @torch.no_grad()
+    def loadAlignCameras(self, if_vis_train=False, if_vis_test=False, camera_uid_list=[], iteration=0):
         ##############################################################################################################
         if if_vis_test:
             self.train_cameras = torch.load('./opt_cams.pt')
@@ -189,9 +252,11 @@ class Scene:
         center_GT = torch.stack([camera.get_camera_center() for camera in self.get_unnoisy_TrainCameras()], dim=0) # [N,3]
         try:
             sim3 = procrustes_analysis(center_GT,center_pred)
+            self.sim3 = sim3
         except:
             print("warning: SVD did not converge...")
-            sim3 = edict(t0=0,t1=0,s0=1,s1=1,R=torch.eye(3,device=opt.device))
+            sim3 = edict(t0=0,t1=0,s0=1,s1=1,R=torch.eye(3,device='cuda'))
+            self.sim3 = sim3
         if if_vis_train:
             center_pred2gt = (center_pred - sim3.t1) / sim3.s1@sim3.R.t()*sim3.s0 + sim3.t0
             center_gt2pred = (center_GT - sim3.t0) / sim3.s0@sim3.R*sim3.s1 + sim3.t1
@@ -210,11 +275,9 @@ class Scene:
                 mask = [True if camera.uid in camera_uid_list else False for camera in self.getTrainCameras()]
                 return torch.cat((R_gt.transpose(1, 2), t_gt), dim=-1)[mask], torch.cat((R_pred2gt.transpose(1, 2), t_pred2gt), dim=-1)[mask]
             return torch.cat((R_gt.transpose(1, 2), t_gt), dim=-1), torch.cat((R_pred2gt.transpose(1, 2), t_pred2gt), dim=-1)
-            for R, t, camera in zip(R_pred2gt, t_pred2gt, self.train_cameras[1.]):
-                camera.reset_extrinsic(R.cpu().numpy(), t.cpu().numpy())
         ##############################################################################################################
 
-        if if_vis_test:
+        if if_vis_test and False:
             center_GT = torch.stack([camera.get_camera_center() for camera in self.getTestCameras()], dim=0) # [N,3]
             center_gt2pred = (center_GT - sim3.t0) / sim3.s0@sim3.R*sim3.s1 + sim3.t1
             R_gt = torch.stack([camera.get_w2c[:3, :3].t() for camera in self.getTestCameras()], dim=0)
