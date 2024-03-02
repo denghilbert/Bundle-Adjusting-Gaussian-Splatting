e54bd13369a3b7df5e39a9948b2313b1fe331351
denghilbert <971431670@qq.com>
2024-03-02 13:32:47 -0500

    Keep test-time optimization the same lr

--- scene/__init__.py
+++ scene/__init__.py
@@ -178,8 +178,8 @@ class Scene:
 
         self.optimizer_rotation = torch.optim.Adam(l_rotation, eps=1e-15)
         self.optimizer_translation = torch.optim.Adam(l_translation, eps=1e-15)
-        self.scheduler_rotation = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_rotation, milestones=[10000], gamma=0.1)
-        self.scheduler_translation = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_translation, milestones=[10000], gamma=0.1)
+        self.scheduler_rotation = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_rotation, milestones=[10000], gamma=1)
+        self.scheduler_translation = torch.optim.lr_scheduler.MultiStepLR(self.optimizer_translation, milestones=[10000], gamma=1)
 
         self.optimizer_rotation_test = torch.optim.Adam(l_rotation_test, eps=1e-15)
         self.optimizer_translation_test = torch.optim.Adam(l_translation_test, eps=1e-15)
