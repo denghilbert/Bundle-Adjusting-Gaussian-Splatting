0cbdabb3cf2ad65f150bf765ae9aa63fcb1414cd
denghilbert <971431670@qq.com>
2024-05-04 13:24:03 -0400

    Add baseline script

---
+++ baseline1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the path where you want to list directories
+path_to_directories="dataset/FisheyeNeRF"
+
+# Iterate over each directory in the specified path
+for dir in "$path_to_directories"/*; do
+    # Check if it's a directory
+    if [ -d "$dir" ]; then
+        name=$(basename "$dir")
+        #echo python train_ref.py -s $dir -m "baseline/ref_$name" --r_t_noise 0.0 0.0 --test_iterations 1 7000 10000 15000 20000 25000 30000 40000 50000 60000 --save_iterations 7000 10000 20000 30000 40000 50000 60000 --iterations 30000 --eval --r_t_lr 0.01 0.05
+        #python train_ref.py -s $dir -m "baseline/ref_$name" --r_t_noise 0.0 0.0 --test_iterations 1 7000 10000 15000 20000 25000 30000 40000 50000 60000 --save_iterations 7000 10000 20000 30000 40000 50000 60000 --iterations 30000 --eval --r_t_lr 0.01 0.05
+        echo python train_vanilla.py -s $dir -m "baseline/grid_$name" --r_t_noise 0.0 0.0 --test_iterations 1 7000 10000 15000 20000 25000 30000 40000 50000 60000 --save_iterations 7000 10000 20000 30000 40000 50000 60000 --iterations 30000 --eval --r_t_lr 0.01 0.05 --opt_distortion
+        python train_vanilla.py -s $dir -m "baseline/grid_$name" --r_t_noise 0.0 0.0 --test_iterations 1 7000 10000 15000 20000 25000 30000 40000 50000 60000 --save_iterations 7000 10000 20000 30000 40000 50000 60000 --iterations 30000 --eval --r_t_lr 0.01 0.05 --opt_distortion
+    fi
+done
